570679a88082b4e6701fbd092e37981cf98a5e4c
Igweonu Ebuka <ebuka7032@gmail.com>
2025-05-13 22:24:47 +0100

    add font family

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,25 +1,31 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css';
+import { Autoplay} from 'swiper/modules';
+
 
 export default ({data}) => {
     return (
       <a>
         <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
+        spaceBetween={30}
+        slidesPerView={2}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
       >
         {data.map((item)=> (
           <div key={item.id}>
             <SwiperSlide>
               <div>
+                
                 <img src='https://res.cloudinary.com/djvdendlh/image/upload/v1743370342/gratisography-cool-pineapple-free-stock-photo_o9m0wn.png' alt=''/>
-                <h4>{item.title}</h4>
                 <p className='slide_details'>
-                  <span>{item.category}</span>
+                  <span className='tag'>{item.category}</span>
                   {/* <span>{item.published_date}</span> */}
                 </p>
+                <h4>{item.title}</h4>
               </div>
             </SwiperSlide>
           </div>
